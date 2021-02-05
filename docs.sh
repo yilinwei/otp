062432ba456b7fa3bfbff6393a587dd4a777600d
yilinwei <yilin.wei10@gmail.com>
2021-02-05 19:02:52 +0000

    Fix shell script

--- docs.sh
+++ docs.sh
@@ -2,31 +2,31 @@
 
 set -euo pipefail
 
+doc_directory=$1
+scribble_file=$2
+github_token=$3
+
 # This part of the script, which publishes the scribble docs to Github pages,
 # is adapted from Alexis King’s original scripts for Travis CI. Unfortunately,
 # we cannot infer very much from the package structure so we rely on the
 # the user to set the paths.
-if [ -n "$INPUT_DOC_DIRECTORY" ] && [ -n "$INPUT_SCRIBBLE_FILE" ] \
-       && [ -n "$INPUT_MAIN_REF" ] && [ -n "$INPUT_GITHUB_TOKEN" ]; then
-    if [[ -z "$GITHUB_BASE_REF" ]] && [ "$INPUT_MAIN_REF" == "$GITHUB_REF" ]; then
-        echo "Uploading documentation under ${INPUT_DOC_DIRECTORY}..."
-	raco scribble +m --htmls \
-	     --redirect http://pkg-build.racket-lang.org/doc/ \
-	     --dest ./docs \
-	     "${INPUT_DOC_DIRECTORY}/scribblings/${INPUT_SCRIBBLE_FILE}.scribl"
-	# Here we create a /new/ repository with no history where the scribble docs are generated.
-	cd docs || exit 1
-	# Sometimes the scribble command will create a subfolder, but Github doesn't like it.
-	# This is a noop in the case that there isn't one.
-	cd $(find . -maxdepth 1 -type d | tail -n1) || exit 1
-        git config --global user.email $(git show -s --format=format:%ae) 
-        git config --global user.name $(git show -s --format=format:%an)
-	git init
-	git add .
-	git commit -m 'Deploy to Github Pages'
-	# Rarely do we want to actually save the history of the Github Pages — force pushing wipes the history.
-	git push --force  \
-	    "https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" \
-	    master:gh-pages
-    fi
+if [ -n "$doc_directory" ] && [ -n "$scribble_file" ] && [ -n "$github_token" ]; then
+    raco scribble +m --htmls \
+	 --redirect http://pkg-build.racket-lang.org/doc/ \
+	 --dest ./docs \
+	 "${doc_directory}/scribblings/${scribble_file}.scribl"
+    # Here we create a /new/ repository with no history where the scribble docs are generated.
+    cd docs || exit 1
+    # Sometimes the scribble command will create a subfolder, but Github doesn't like it.
+    # This is a noop in the case that there isn't one.
+    cd $(find . -maxdepth 1 -type d | tail -n1) || exit 1
+    git config --global user.email $(git show -s --format=format:%ae) 
+    git config --global user.name $(git show -s --format=format:%an)
+    git init
+    git add .
+    git commit -m 'Deploy to Github Pages'
+    # Rarely do we want to actually save the history of the Github Pages — force pushing wipes the history.
+    git push --force  \
+	"https://${GITHUB_ACTOR}:${github_token}@github.com/${GITHUB_REPOSITORY}.git" \
+	master:gh-pages
 fi
