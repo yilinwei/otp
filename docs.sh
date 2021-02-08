f843e6e123c88f0582894c3e98e83c689ba161ee
Yilin Wei <yilin.wei10@gmail.com>
2021-02-08 20:31:29 +0000

    Update docs.sh

--- docs.sh
+++ docs.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 doc_directory=$1
 scribble_file=$2
 github_token=$3
+doc_branch='gh:pages'
 
 # This part of the script, which publishes the scribble docs to Github pages,
 # is adapted from Alexis King’s original scripts for Travis CI. Unfortunately,
@@ -20,13 +21,16 @@ if [ -n "$doc_directory" ] && [ -n "$scribble_file" ] && [ -n "$github_token" ];
     # Sometimes the scribble command will create a subfolder, but Github doesn't like it.
     # This is a noop in the case that there isn't one.
     cd $(find . -maxdepth 1 -type d | tail -n1) || exit 1
+    
+    # Take the /last/ commit user
     git config --global user.email $(git show -s --format=format:%ae) 
     git config --global user.name $(git show -s --format=format:%an)
     git init
     git add .
-    git commit -m 'Deploy to Github Pages'
+    git commit -m 'Deploy gh:pages from CI'
+    
     # Rarely do we want to actually save the history of the Github Pages — force pushing wipes the history.
     git push --force  \
 	"https://${GITHUB_ACTOR}:${github_token}@github.com/${GITHUB_REPOSITORY}.git" \
-	master:gh-pages
+	"master:${doc_branch}"
 fi
