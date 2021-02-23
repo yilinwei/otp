0757167eac914c45a756c090c4bdf5410080c145
Yilin Wei <yilin@kebab-ca.se>
2021-02-23 21:12:54 +0000

    Update info.rkt

--- typed-otp-lib/info.rkt
+++ typed-otp-lib/info.rkt
@@ -2,6 +2,6 @@
 
 (define collection "otp")
 (define version "0.1")
-(define build-deps '("rackunit-lib"))
-(define deps '("otp-lib" "typed-racket-lib"))
+(define build-deps '("rackunit-typed"))
+(define deps '("base" "crypto-lib" "otp-lib" "typed-racket-lib"))
 (define name "otp")
