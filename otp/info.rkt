e7a96792badd7864f1a7c32394053e866ccd9753
Sam Tobin-Hochstadt <samth@indiana.edu>
2022-05-16 16:15:19 -0400

    Fix dependencies.

--- otp/info.rkt
+++ otp/info.rkt
@@ -2,6 +2,8 @@
 
 (define version "0.1")
 (define collection "otp")
-(define deps '("base" "otp-lib" "typed-otp-lib" "otp-doc"))
+(define deps '("crypto-lib"
+               "rackunit-lib"
+               "base" "otp-lib" "typed-otp-lib" "otp-doc"))
 (define implies '("otp-lib" "typed-otp-lib" "otp-doc"))
 (define name "otp")
